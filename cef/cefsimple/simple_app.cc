000c846fbf1ab2471d450928274cf22cccb57965
stofte <stofte@gmail.com>
2015-11-08 13:40:40 +0100

    launch cef

--- cef/cefsimple/simple_app.cc
+++ cef/cefsimple/simple_app.cc
@@ -19,11 +19,15 @@ void SimpleApp::OnContextInitialized() {
 
   // Information used when creating the native window.
   CefWindowInfo window_info;
+  CefRefPtr<CefCommandLine> command_line = CefCommandLine::GetGlobalCommandLine();
 
 #if defined(OS_WIN)
   // On Windows we need to specify certain flags that will be passed to
   // CreateWindowEx().
   window_info.SetAsPopup(NULL, "cefsimple");
+  if (command_line->GetSwitchValue("enableui").empty()) {
+	  window_info.style = WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS;
+  }
 #endif
 
   // SimpleHandler implements browser-level callbacks.
@@ -36,11 +40,10 @@ void SimpleApp::OnContextInitialized() {
 
   // Check if a "--url=" value was provided via the command-line. If so, use
   // that instead of the default URL.
-  CefRefPtr<CefCommandLine> command_line =
-      CefCommandLine::GetGlobalCommandLine();
+  
   url = command_line->GetSwitchValue("url");
   if (url.empty())
-    url = "http://www.google.com";
+    url = "about:blank";
 
   // Create the first browser window.
   CefBrowserHost::CreateBrowser(window_info, handler.get(), url,
--- index.js
+++ index.js
@@ -3,18 +3,13 @@ var childProcess = require('child_process');
 var phantomjs = require('phantomjs');
 var Chrome = require('chrome-remote-interface');
 
-var cefPath = path.join(__dirname, 'cef/cefsimple.exe');
-var chromePath = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe';
+var cefPath = path.join(__dirname, 'cef/cefsimple/Release/cefsimple.exe');
 var cefArgs = [
     '--url=about:blank',
+    '--enableui=true',
     '--remote-debugging-port=9222'
 ];
-var chromeArgs = [
-    '--incognito',
-    '--remote-debugging-port=9222'
-];
-//console.log(phantomjs.path, childArgs[0], childArgs[1], childArgs[2], childArgs[3]);
-var proc = childProcess.execFile(chromePath, chromeArgs, function(err, stdout, stderr) {
+var proc = childProcess.execFile(cefPath, cefArgs, function(err, stdout, stderr) {
     console.log('stdout', stdout);
     console.log('stderr', stderr);
 });
